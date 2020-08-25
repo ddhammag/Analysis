e81a2c6caf6718555b19bd7c765b0abb3a7c940d
ddhammag <ddescw@gmail.com>
2020-08-25 12:50:34 +0200

    corrected some errors

--- jetAnalyzer/plugins/jetAnalyzer.cc
+++ jetAnalyzer/plugins/jetAnalyzer.cc
@@ -69,22 +69,22 @@ jetAnalyzer::analyze(const edm::Event& iEvent,
   int ijet = 0;
    // fill pat muon histograms
    for (auto j = jet->cbegin(); j != jet->cend(); ++j) {
-      histContainer_["muonPt"] ->Fill(j->pt());
-      histContainer_["muonEta"]->Fill(j->eta());
-      histContainer_["muonPhi"]->Fill(j->phi());
+      //histContainer_["muonPt"] ->Fill(j->pt());
+      //histContainer_["muonEta"]->Fill(j->eta());
+      //histContainer_["muonPhi"]->Fill(j->phi());
 
       if( j->pt()>20){
       printf("jet  with pt %5.1f (raw pt %5.1f), eta %+4.2f, btag CSV %.3f, CISV %.3f, pileup mva disc %+.2f\n",
             j->pt(), j->pt()*j->jecFactor("Uncorrected"), j->eta(), std::max(0.f,j->bDiscriminator("combinedSecondaryVertexBJetTags")), std::max(0.f,j->bDiscriminator("combinedInclusiveSecondaryVertexBJetTags")), j->userFloat("pileupJetId:fullDiscriminant"));
-       /*
+       
          if ((++ijet) == 1) { // for the first jet, let's print the leading constituents
-            std::vector daus(j->daughterPtrVector());
+            std::vector<reco::CandidatePtr> daus(j->daughterPtrVector());
             std::sort(daus.begin(), daus.end(), [](const reco::CandidatePtr &p1, const reco::CandidatePtr &p2) { return p1->pt() > p2->pt(); }); // the joys of C++11
             for (unsigned int i2 = 0, n = daus.size(); i2 < n && i2 <= 3; ++i2) {
                 const pat::PackedCandidate &cand = dynamic_cast<const pat::PackedCandidate &>(*daus[i2]);
                 printf("         constituent %3d: pt %6.2f,  pdgId %+3d\n", i2,cand.pt(),cand.pdgId());
             }
-        }*/
+        }
       }
    }
  
